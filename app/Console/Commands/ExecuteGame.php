b5f0ce2934b72497c5af1ba274954f9b948ad282
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2019-10-27 12:17:24 -0300

    - Updated the ExecuteGame class adding some code standards; - Updating the composer.lock file;

--- app/Console/Commands/ExecuteGame.php
+++ app/Console/Commands/ExecuteGame.php
@@ -1,23 +1,28 @@
-<?php 
-
+<?php
+/*
+ * A custom command used to invoke the 'Game Play'.
+ *
+ * Command line:
+ * php artisan game:execute
+ *
+ * @author Guilherme Borge Bastos
+ * @data   10/27/2019
+*/
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Game;
 
 class ExecuteGame extends Command {
-    
+
+    private $game;
+
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
     protected $signature = 'game:execute';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = "Executes the VideoSlots' demo game, developed by Guilherme Borges Bastos. Based on a technical exercise.";
 
     /**
@@ -28,16 +33,16 @@ class ExecuteGame extends Command {
     public function __construct()
     {
         parent::__construct();
+        $this->game = new Game(config(SETTINGS));
     }
 
     /**
-     * Execute the console command.
+     * Execute the console command playing the Game.
      *
      * @return mixed
      */
     public function handle()
     {
-
-        $this->info('hello VideoSlots! Here I go...');
+        $this->game->play();
     }
 }
