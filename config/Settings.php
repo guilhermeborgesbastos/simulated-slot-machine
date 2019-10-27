543c4552a907a072ebc8701d4de98aee4f11db07
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2019-10-27 08:09:07 -0300

    - Created the Seertings file that contains the criterias for the application; - Updated the App.php to include the Seetings.php as an app configuration.

---
+++ config/Settings.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file contains the configurations for the application works. The intent here is to be
+ * able to change the application's metrics without touch the concrete implementation.
+ *
+ * @author Guilherme Borge Bastos
+ * @data   10/27/2019
+*/
+return [
+
+    // The bet's total amount.
+    'bet_amount' => 1,
+
+    // The dimensions of the grid (2d Array) that will contain the random symbols.
+    'grid' => [
+        'rows' => 3,
+        'columns' => 5
+    ],
+
+    // The symbols that are available to randomly fill the grid (2d array).
+    'symbols' => [
+        '9', '10', 'J', 'Q', 'K',
+        'A', 'cat', 'dog', 'monkey', 'bird'
+    ],
+
+    /*
+     * The matrix that contains the payline sequences. A payout happens when 3 or more
+     * consecutive symbols of the same kind are present in a payline, always starting
+     * from the first column (0, 1, 2).
+    */
+    'paylines' => [
+        [0, 3, 6, 9,  12],
+        [1, 4, 7, 10, 13],
+        [2, 5, 8, 11, 14],
+        [0, 4, 8, 10, 12],
+        [2, 4, 6, 10, 14]
+    ],
+
+    /*
+     * The payout matrix, containing the number of consecutive symbols in the grid (2d matrix)
+     * based on the paylines. It has its Key as the number of consecutive symbols and the value
+     * as the percentage of earning over the bet amount.
+    */
+    'payouts' => [
+        3 => 20,
+        4 => 200,
+        5 => 1000
+    ]
+];
+?>
